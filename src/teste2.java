2e51f1647d0da2897fb693b1698704ab8ecb4069
EuRonald123 <ronalds4d123@gmail.com>
2025-01-21 16:22:28 -0300

    testando String

--- src/teste2.java
+++ src/teste2.java
@@ -4,10 +4,16 @@ import TreeAVL.ArvoreAVL;
 public class teste2 {
 	public static void main(String[] args) {
 		ArvoreAVL<Integer> avlTree = new ArvoreAVL<>();
+		ArvoreAVL<String> avlTreeString = new ArvoreAVL<>();
 		
 		int []vet= {46,27,20,15,10,90,98,95};
 		for(int i=0;i<vet.length;i++) {
 			avlTree.insert(vet[i]);
+		}
+
+		String []vetName= {"Ro","rona","ronal","ronald","peituda"};
+		for(int i=0;i<vetName.length;i++) {
+			avlTreeString.insert(vetName[i]);
 		}
 		
         System.out.println("\n***InOrder***\n");
@@ -38,6 +44,32 @@ public class teste2 {
 		}else{
 			System.out.println("***FALSE***");
 		}
+
+
+
+
+		System.out.println("\n**********************************************");
+		System.out.println("\n***InOrder***\n");
+		avlTreeString.printInOrder();
+		System.out.println("\n**********************************************");
+		System.out.println("\n***PosOrder***\n");
+		avlTreeString.printPosOrder();
+
+		avlTreeString.remove("peituda");
+
+		System.out.println("\n*****************************\n");
+		if(avlTreeString.find("peituda")){
+			System.out.println("\n***TRUE***");
+		}else{
+			System.out.println("***FALSE***");
+		}
+
+
+		System.out.println("\n**********************************************");
+		System.out.println("\n***InOrder***\n");
+		avlTreeString.printInOrder();
+		System.out.println("\n**********************************************");
+		System.out.println("\n***PosOrder***\n");
+		avlTreeString.printPosOrder();
 	}
-	
 }
